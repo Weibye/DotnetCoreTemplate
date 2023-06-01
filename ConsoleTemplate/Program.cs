eaa643578cfbea7b3d66bcf57fed744866bec762
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2023-06-01 19:31:15 +0200

    Make the main program slightly more interesting

--- ConsoleTemplate/Program.cs
+++ ConsoleTemplate/Program.cs
@@ -8,12 +8,20 @@ namespace ConsoleTemplate;
 
 /// <summary>
 /// This is the main entrypoint of a console application that uses the class library for functionality.
+/// It prints out a sequence of numbers following the Fibonacci sequence.
 /// </summary>
 public static class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine($"Adding 5 + 3: { Math.Add(5, 3)}");
+        var previousNumber = 0;
+        var currentNumber = 1;
+        for (var i = 0; i < 20; i++)
+        {
+            var result = Math.Add(currentNumber, previousNumber);
+            previousNumber = currentNumber;
+            currentNumber = result;
+            Console.WriteLine($"Fibonacci: { result }");
+        }
     }
 }
