be476ce3dff3c575a10a2af96ab2d002d68d7e91
Andreas Weibye <13300393+Weibye@users.noreply.github.com>
2023-06-01 19:39:12 +0200

    Cleanup of Program

--- ConsoleTemplate/Program.cs
+++ ConsoleTemplate/Program.cs
@@ -1,10 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Math = MathLib.Math;
+﻿using Math = MathLib.Math;
 namespace ConsoleTemplate;
-
-// The code should be microbenchmarked
-// Documentation should be compiled and output
-// It should release?
 
 /// <summary>
 /// This is the main entrypoint of a console application that uses the class library for functionality.
